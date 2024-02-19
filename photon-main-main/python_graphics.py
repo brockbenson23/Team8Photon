ea830f2223b457cfc1ed5d42e9423206afa90419
brock benson <brockbenson@brocks-air.ddns.uark.edu>
2024-02-19 11:06:34 -0600

    bat fixed? + mor. additions

--- photon-main-main/python_graphics.py
+++ photon-main-main/python_graphics.py
@@ -26,32 +26,41 @@ label.grid()
 win.after(3000, win.destroy)
 win.mainloop()
 
+red = '#990000'
+green = '#346C4E'
 
 class Application(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master = master
         self.master.title("Team8Photon")
-        self.frame1 = Frame(master, background='#990000')
+        self.frame1 = Frame(master, background=red)
         self.frame1.place(x=0, y=0, relwidth=0.5,
                           relheight=1.0, anchor="nw")
-        self.frame2 = Frame(master, background="#346C4E",)
+        self.frame2 = Frame(master, background=green)
         self.frame2.place(relx=0.5, y=0, relwidth=0.5,
                           relheight=1.0, anchor="nw")
         self.grid()
         self.createWidgets()
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
 
     def createWidgets(self):
+        self.Label = Label(text='Red Team', bg=red, fg='white')
+        self.Label.grid(row=0, column=0)
+        self.Label = Label(text='Green Team', bg=green, fg='white')
+        self.Label.grid(row=0, column=1)
+
         for i in range(15):
             self.entry = Entry(bg="white", fg="black", bd=0)
-            self.entry.config(highlightbackground="#990000",
-                              highlightcolor="#990000")
-            self.entry.grid(row=i, column=0)
+            self.entry.config(highlightbackground=red,
+                              highlightcolor=red)
+            self.entry.grid(row=i+1, column=0)
         for i in range(15):
             self.entry = Entry(bg="white", fg="black", bd=0)
-            self.entry.config(highlightbackground="#346C4E",
-                              highlightcolor="#346C4E")
-            self.entry.grid(row=i, column=1)
+            self.entry.config(highlightbackground=green,
+                              highlightcolor=green)
+            self.entry.grid(row=i+1, column=1)
 
 
 root = Tk()
--- photon-main-main/run.bat
+++ photon-main-main/run.bat
@@ -1,7 +1,7 @@
 pushd udp_files
 echo "in udp"
-python python_udpserver.py
-python python_udpclient.py
+python python_udpserver.py &
+python python_udpclient.py &
 popd
 python python_graphics.py
 echo "done"
