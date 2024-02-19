6bd9a4f3f04aa781d0ba5024bd90964706fefb4f
CharlesPoolman <157633127+CharlesPoolman@users.noreply.github.com>
2024-02-19 16:47:02 -0600

    Update player.sql

--- photon-main-main/player.sql
+++ photon-main-main/player.sql
@@ -1,13 +1,10 @@
 --Create table
 CREATE TABLE player (
   id INT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
   codename VARCHAR(30)
 );
 
 --Place first record into table
-INSERT INTO player (id, first_name, last_name, codename)
-VALUES (1, 'Jim', 'Strother', 'Opus');
-VALUES (2, 'Charles', 'Poolman', 'Mario');
-
+INSERT INTO player (id, codename)
+VALUES (1, 'Opus');
+VALUES (2, 'Mario');
