6bca095bd32f3230ee4fa09fc54859971376c172
brock benson <brockbenson@brocks-air.ddns.uark.edu>
2024-02-29 14:18:31 -0600

    bat file

--- photon-main-main/run.bat
+++ photon-main-main/run.bat
@@ -1,13 +1,10 @@
-@echo off
 echo "in udp"
-<<<<<<< HEAD
 python3 python_udpserver.py &
 python3 python_udpclient.py &
 popd
-=======
+
 start python3 python_udpserver.py
 start python3 python_udpclient.py
->>>>>>> 6bd9a4f3f04aa781d0ba5024bd90964706fefb4f
 python3 python_graphics.py
 echo "done"
 
