50f18892330c35a6e6a7aebf9b8e43d750c48afa
CharlesPoolman <157633127+CharlesPoolman@users.noreply.github.com>
2024-03-04 16:07:02 -0600

    Update python_graphics.py
    fixed button again

--- photon-main-main/python_graphics.py
+++ photon-main-main/python_graphics.py
@@ -172,7 +172,7 @@ class Application(Frame):
     def createButton(self):
         self.button = Button(text="Submit ID", command=self.addData)
         self.button.grid(row=18, column=1, columnspan=2)
-        self.button = Button(text=“F12”, command=self.addData)
+        self.button = Button(text="F12: Clear Players", command=self.addData)
         self.button.grid(row=19, column=1, columnspan=2)
 
     def addData(self) -> str:
