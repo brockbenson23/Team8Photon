da0d5b884887c15a904076ab9ff1b36646d4e929
CliffordLew <clifski@gmail.com>
2024-04-02 14:41:50 -0500

    started game funcitons file

---
+++ photon-main-main/python_gamefuncs.py
@@ -0,0 +1,18 @@
+import os
+import python_supabase
+
+class Player():
+    def __init__(self) -> None:
+
+        return
+
+
+class GameInteractions():
+    ## player hitting player parameters = (playerID of victim, playerID of shooter)
+    def playerHit(ID1, ID2) -> None:
+        return
+
+
+    ## player hitting base
+    def playerBase(ID) -> None:
+        return
