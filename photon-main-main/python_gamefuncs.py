f86d103817a9f10299df78bef0e2d12a94e3b3d3
CliffordLew <clifski@gmail.com>
2024-04-03 19:46:54 -0500

    added very basic methods, havent tested if they work yet

--- photon-main-main/python_gamefuncs.py
+++ photon-main-main/python_gamefuncs.py
@@ -3,14 +3,25 @@ import python_supabase
 
 ## IDEA FOR FILE : Declare player objects when game begins, use these objects
 ## during the game and update supabase and graphics every interaction
+class Team():
+    points = 0
+    ## will initialize a green and red team
+    def __init__(self, color) -> None:
+        self.players = []
+        self.color = color
+    
+    def addPlayer(self, player) -> None:
+        self.players.append(player)
 
 class Player():
+    points = 0
     def __init__(self, pID) -> None:
         ## take data from supabase
-        playerID = pID
-        hardwareID = -1 ## implement when we find out what this is
-        codeName = python_supabase.Database.fetch_name(pID)
-        points = 0 ## doesnt need to be on supabase
+        self.playerID = pID
+        self.hardwareID = -1 ## implement when we find out what this is
+        self.codeName = python_supabase.Database.fetch_name(pID)
+        if ((pID % 2) == 1): self.color = "RED" ## IF ID == ODD -> RED TEAM
+        else: self.color = "GREEN" ## IF ID == EVEN -> GREEN TEAM
     
     def styleB(self) -> None:
         ## placeholder because idk where to find stylized B
@@ -21,11 +32,15 @@ class Player():
 
 
 class GameInteractions():
-    ## player hitting player parameters = (playerID of victim, playerID of shooter)
-    def playerHit(ID1, ID2) -> None:
+    ## player is person who hit someone
+    def playerHit(player, team) -> None:
+        team.points += 10
+        player.points += 10
         return
 
 
     ## player hitting base
-    def playerBase(ID) -> None:
+    def playerBase(player, team) -> None:
+        player.styleB()
+        team.points += 0 ## PLUG IN POINTS WHEN YOU FIND OUT HOW MANY THEY GET PER BASE
         return
