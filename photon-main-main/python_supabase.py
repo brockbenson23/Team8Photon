6866983eca8c3a2e22bd302028b03a6232a6ad23
CliffordLew <clifski@gmail.com>
2024-04-09 15:55:15 -0500

    fixed what i messed up last commit

--- photon-main-main/python_supabase.py
+++ photon-main-main/python_supabase.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 from typing import Dict
 from typing import List
+import udp_files.python_udpclient
 
 ## hopefully solves __pycache__ problem
 PYTHONDONTWRITEBYTECODE = "TRUE"
@@ -16,6 +17,7 @@ class Database:
     @staticmethod
     def addData(values: Dict[int, str]) -> Dict[int, str]:
         print("submitting id...")
+        entries = {}
         for id, name in values.items():
             print('id = ', id, ' name = ', name)
 
@@ -24,10 +26,19 @@ class Database:
             if existing_entry is not None:
                 print(
                     f"ID {id} already exists in the table. Codename: {existing_entry['codename']}")
-                return {id: existing_entry['codename']}
+                entries[id] = existing_entry['codename']
             else:
-                Database.add_entry_to_player_table(Database.supabase, id, name)
-                return {0: ''}
+                if name != '':
+                    Database.add_entry_to_player_table(id, name)
+        return entries
+    
+    @staticmethod
+    def addHardware(values: Dict[int, int]):
+        load_dotenv()
+
+        for id, name in values.items():
+            print('id = ', id, ' name = ', name)
+            Database.add_hardware(Database.supabase, id, name)
 
     @staticmethod
     def check_existing_entry(supabase, id):
@@ -44,6 +55,13 @@ class Database:
             {'id': id, 'codename': codename}).execute()
         print(data)
 
+    @staticmethod
+    def add_hardware(supabase, id, equipment_id):
+        data = supabase.table('player').update(
+            {'equipment_id': equipment_id}).eq('id', id).execute()
+        udp_files.python_udpclient.transmitEquipmentCode(equipment_id)
+        print(data)
+        
     @staticmethod
     def update_name(id, newname):
         data = Database.supabase.table('player')
