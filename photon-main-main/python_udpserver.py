77a5df657144692898ce9dc40c9ef738b4c98f66
brockbenson23 <brockbenson23@gmail.com>
2024-04-28 22:17:05 -0500

    it run now

--- photon-main-main/python_udpserver.py
+++ photon-main-main/python_udpserver.py
@@ -4,13 +4,15 @@ receivePort = 7501
 broadcastPort = 7500
 bufferSize = 1024
 
+
 class Socket:
 
     def __init__(self) -> None:
         # Setting up broadcast socket
         self.UDPBroadcastSocket = socket.socket(
             socket.AF_INET, socket.SOCK_DGRAM)
-        self.UDPBroadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.UDPBroadcastSocket.setsockopt(
+            socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         # Create a datagram socket for receiving
         self.UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.UDPServerSocket.bind((localIP, receivePort))
@@ -18,21 +20,25 @@ class Socket:
 
     def recvfrom(self):
         return self.UDPServerSocket.recvfrom(bufferSize)
-    
+
     def transmitCode(self, code):
         bytesToSend = str.encode(code)
-        self.UDPBroadcastSocket.sendto(bytesToSend, ('<broadcast>', broadcastPort))
+        self.UDPBroadcastSocket.sendto(
+            bytesToSend, ('<broadcast>', broadcastPort))
         self.UDPBroadcastSocket.close()
 
     def transmitCode(self, code):
         bytesToSend = str.encode(code)
-        self.UDPBroadcastSocket.sendto(bytesToSend, ('<broadcast>', broadcastPort))
+        self.UDPBroadcastSocket.sendto(
+            bytesToSend, ('<broadcast>', broadcastPort))
         self.UDPBroadcastSocket.close()
 
     def transmitCode(self, code):
         bytesToSend = str.encode(code)
-        self.UDPBroadcastSocket.sendto(bytesToSend, ('<broadcast>', broadcastPort))
+        self.UDPBroadcastSocket.sendto(
+            bytesToSend, ('<broadcast>', broadcastPort))
         self.UDPBroadcastSocket.close()
+
 
 def decipherMsg(self, serverMsg):
     colon = serverMsg.find(':')
@@ -50,22 +56,22 @@ def decipherMsg(self, serverMsg):
         print("player with id {} has hit player with id {}".format(str1, str2))
 
     return str1, str2
+
+
 if __name__ == "__main__":
     s = Socket()
 # Listen for incoming datagrams
-while (True):
-    bytesAddressPair = s.recvfrom()
+    while (True):
+        bytesAddressPair = s.recvfrom()
 
-    # deciper message
-    serverMsg = str(bytesAddressPair[0])
-    address = bytesAddressPair[1]  # may need to stringify
-    print("Message from Client:{}".format(serverMsg))
-    print("Client IP Address:{}".format(address))
+        # deciper message
+        serverMsg = str(bytesAddressPair[0])
+        address = bytesAddressPair[1]  # may need to stringify
+        print("Message from Client:{}".format(serverMsg))
+        print("Client IP Address:{}".format(address))
 
-    # separate message "1:2" into str1 = 1 and str2 = 2
-    str1, str2 = decipherMsg(serverMsg)
+        # separate message "1:2" into str1 = 1 and str2 = 2
+        str1, str2 = decipherMsg(serverMsg)
 
-    s.transmitCode(str1)
-
-
+        s.transmitCode(str1)
 
